cb29ee84a1b43315e233b1bc92cbe3320bf22563
Mikkel RINGAUD <mikkel@milescode.dev>
2023-07-29 20:55:18 +0200

    refactor: `utils/json`
    - add comments
    - rename exported `parse` and `stringify` functions
    - more typings for `isSerializable`
    - add more context

--- src/utils/json.ts
+++ src/utils/json.ts
@@ -3,35 +3,38 @@
 // Do not use any browser or node-specific API!
 // -------------------------------------------------------------
 
-// NOTE: We should store the methods of the `JSON` object
+// NOTE: We should store these methods from `JSON`
 // since they can be overridden by the client code.
-export const parse     = JSON.parse;
-export const stringify = JSON.stringify;
+export const parseJSON = JSON.parse;
+export const stringifyJSON = JSON.stringify;
 
-function isDOMNode (obj) {
-    if (typeof Node === 'object')
-        return obj instanceof Node;
+const isDOMNode = (data: Record<string, unknown>): boolean => {
+  // Check if we have access to `Node`, if yes then we can directly check instance.
+  if (typeof Node === "object")
+    return data instanceof Node;
 
-    return typeof obj.nodeType === 'number' && typeof obj.nodeName === 'string';
-}
+  // When no access, we simply check for basic properties in `Node` objects.
+  return typeof data.nodeType === "number" && typeof data.nodeName === "string";
+};
 
-function isJQueryObj (obj) {
-    return !!(obj && obj.jquery);
-}
+const isJQueryObject = (data: Record<string, unknown>) => !!(data && data.jquery);
 
-export function isSerializable (value) {
-    if (value) {
-        // NOTE: jquery object, DOM nodes and functions are disallowed obj types because we can't serialize them correctly
-        if (typeof value === 'function' || isJQueryObj(value) || isDOMNode(value))
-            return false;
+// Functions and objects can be passed as parameter.
+type SerializableValueParameter = (() => unknown) | Record<string, unknown> | undefined;
+export const isSerializable = (value: SerializableValueParameter) => {
+  if (!value) return true;
 
-        if (typeof value === 'object') {
-            for (const prop in value) {
-                if (value.hasOwnProperty(prop) && !isSerializable(value[prop])) // eslint-disable-line no-prototype-builtins
-                    return false;
-            }
-        }
+  // NOTE: JQuery object, DOM nodes and functions are disallowed object
+  // types because we can't serialize them correctly.
+  if (typeof value === "function" || isJQueryObject(value) || isDOMNode(value))
+    return false;
+
+  if (typeof value === "object") {
+    for (const prop in value) {
+      if (value.hasOwnProperty(prop) && !isSerializable(value[prop] as SerializableValueParameter)) // eslint-disable-line no-prototype-builtins
+        return false;
     }
+  }
 
-    return true;
-}
+  return true;
+};
